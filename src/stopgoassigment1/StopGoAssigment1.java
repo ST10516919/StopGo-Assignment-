e7f5af8b5ff75e001212a834158bc735adbd3a62
ST10516919 <ST10516919@myemeris.edu.za>
2026-02-26 08:33:06 +0200

    message

---
+++ src/stopgoassigment1/StopGoAssigment1.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stopgoassigment1;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class StopGoAssigment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("                                                   ");
+        System.out.println("o o o o o o o o o o o o o o o o o o o o o o o      ");
+        System.out.println("                                                   ");
+        System.out.println("                ------------                       ");
+        System.out.println("                                                   ");
+        System.out.println("     *******                  XXXXXXXX             ");
+        System.out.println("   *         *              X          X           ");
+        System.out.println("  *  S T O P  *            X     GO     X          ");
+        System.out.println("   *         *              X          X           ");
+        System.out.println("    *********                 XXXXXXXX             ");
+        System.out.println("        **                       XX                ");
+        System.out.println("        **                       XX                ");
+        System.out.println("        **                       XX                ");
+        System.out.println("        **                       XX                ");
+        System.out.println("       ****                     XXXX               ");
+        System.out.println("    **********               XXXXXXXXXX            ");
+        System.out.println("|-----------------------------------------|        ");
+        System.out.println("|-----------------------------------------|        ");
+        
+        
+    }
+    
+}
